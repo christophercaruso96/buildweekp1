eb2384eecb43e3bec98b36dfc0499e5b31a01951
christophercaruso96 <christophercaruso96@outlook.com>
2024-03-21 16:01:33 +0100

    risolto bug port portScanning

--- scripts/portScanning.py
+++ scripts/portScanning.py
@@ -22,15 +22,16 @@ def is_valid_ip(ip_address):
     
 #funzione per la scansione multiporta su un target specifico
 def scanning_port(ip, min_port, max_port, protocol_in, format):
-    #verifica del protocollo richiesto per la creazione dell'istanza socket 
-    if(protocol_in == "TCP" or protocol_in == "tcp"):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    elif(protocol_in == "UDP" or protocol_in == "udp"):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    else:
-        print ("Il protocollo richiesto non è valido!\n")
-    #iterazione dalla porta minima alla porta massima richiesta
     for port in range(low_port, high_port + 1):
+        #verifica del protocollo richiesto per la creazione dell'istanza socket 
+        if(protocol_in == "TCP" or protocol_in == "tcp"):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        elif(protocol_in == "UDP" or protocol_in == "udp"):
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        else:
+            print ("Il protocollo richiesto non è valido!\n")
+            return 0
+        #iterazione dalla porta minima alla porta massima richiesta
         #esegue la connessione porta per porta
         status = s.connect_ex((ip_target, port))
         #se la connessione è OK stampa OPEN altrimenti CLOSED
@@ -42,8 +43,8 @@ def scanning_port(ip, min_port, max_port, protocol_in, format):
         else:
             if(format == 2 or format == 3):
                 print ('Porta ', port, ' ', protocol_in, ' CLOSED')
-    #chiusura della connessione
-    s.close()
+        #chiusura della connessione
+        s.close()
 
 ### DICHIARAZIONE VARIABILI E CHECK INPUT UTENTE
 
@@ -103,4 +104,3 @@ print ('Scanning host ', ip_target, ' from port ', low_port, ' to port ', high_p
 #chiamata funzione per la scansione sia per porte TCP che per porte UDP
 scanning_port(ip_target, low_port, high_port, "TCP", choise_out)
 scanning_port(ip_target, low_port, high_port, "UDP", choise_out)
-
